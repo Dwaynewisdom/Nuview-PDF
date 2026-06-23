f8d1bbfb643c4399bf5ebb929ba8be400ab6b94c
Dwaynewisdom <Dwaynewisdom5@gmail.com>
2026-06-23 13:11:35 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './navbar.jsx'
 import App from './App.jsx'
+import Merge from './Pages/merge.jsx'
+import Image from './Pages/Image.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/merge" element={<Merge />} />
+        <Route path="/Image" element={<Image/>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )

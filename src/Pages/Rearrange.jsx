7f5dfa24a29fda6c7dfe092057f5d6ee29081a54
Dwaynewisdom <Dwaynewisdom5@gmail.com>
2026-06-26 09:43:39 -0400

    Create Rearrange.jsx

---
+++ src/Pages/Rearrange.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { PageSizes } from 'pdf-lib'
+
+function Rearrange() {
+    const newPage1 = pdfDocEncodingDecode.addpage(PageSizes.A7)
+    const newPage2 = pdfDocEncodingDecode.addPage(PageSizes.letter)
+    const newPage3 = pdfDoc
+  return (
+    <div>Rearrange</div>
+  )
+}
+
+export default Rearrange

4061b9d91458c9342abb9a2d2b5131ad29215313
Dwaynewisdom <Dwaynewisdom5@gmail.com>
2026-06-27 16:51:13 -0500

    Update navbar.jsx

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
             <p className="text-lg text-white/90 mt-1">Your PDFs, through a NU lens.</p>
             <ul className='flex gap-3 ml-14 mt-2'>
                 <li className="w-7 h-7 mt-">
-                <a href ="https://nuview.it.com/" target='_blank'><img src="/logo.png"></img></a>
+                <a href ="https://nuview.it.com/" target='_blank'><img src="https://cdn.jsdelivr.net/gh/Dwaynewisdom/Portfolio-New@8ba4dfe353b3e5a8e349fa9fb4818e5c52af1e58/Images/logo.png"></img></a>
                 </li>
                 <li className="w-7 h-7 mt-">
                     <a href="https://www.linkedin.com/feed/" target='_blank'><img src="https://cdn.jsdelivr.net/gh/Dwaynewisdom/Portfolio-New@d0da4c8807943da012923a3db35062b9f25efcc4/instagram.png"></img></a>

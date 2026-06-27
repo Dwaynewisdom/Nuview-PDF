904a6d23d7beeffd16eee095c172328ddf17c7f6
Dwaynewisdom <Dwaynewisdom5@gmail.com>
2026-06-27 17:03:33 -0400

    Update navbar.jsx

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <>
     <nav className='m-5 border-2 bg-orange-700 border-gray-300 shadow-lg shadow-black/20 rounded-2xl p-6 flex flex-row items-center justify-between gap-4'>
         <div>
-                <a href = "/" className="text-4xl font-bold text-white" style={{ fontFamily: 'Poppins' }}>NuView PDF</a>
+                <Link to = "/" className="text-4xl font-bold text-white" style={{ fontFamily: 'Poppins' }}>NuView PDF</Link>
             <p className="text-lg text-white/90 mt-1">Your PDFs, through a NU lens.</p>
             <ul className='flex gap-3 ml-14 mt-2'>
                 <li className="w-7 h-7 mt-">

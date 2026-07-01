3780fd46ef6e08ce13e21caadc2302b8d4bea629
Dwaynewisdom <Dwaynewisdom5@gmail.com>
2026-07-01 18:56:23 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Merge from './Pages/merge.jsx'
 import Image from './Pages/Image.jsx'
 import PDFtooffice from './Pages/PDFtooffice.jsx'
 import PdfWatermarker from './Pages/watermark.jsx'
+import Editpdf from './Pages/Editpdf.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')).render(
             <Route path="/Image" element={<Image/>} />
             <Route path="/PDFtooffice" element={<PDFtooffice/>} />
             <Route path="/PdfWatermarker" element={<PdfWatermarker/>} />
+            <Route path="/Editpdf" element={<Editpdf/>} />
           </Routes>
         </main>
       </div>
